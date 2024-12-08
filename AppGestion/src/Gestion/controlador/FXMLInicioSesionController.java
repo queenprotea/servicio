72173b2cf0137d7802399ca6f297aed24706aae7
QueenProtea <yizuzelbautista@gmail.com>
2024-12-08 12:51:07 -0600

    khjk

---
+++ AppGestion/src/Gestion/controlador/FXMLInicioSesionController.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package Gestion.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author lolll
+ */
+public class FXMLInicioSesionController implements Initializable {
+
+    @FXML
+    private TextField TFUsuario;
+    @FXML
+    private TextField TFContrasena;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void clickIniciarSesion(ActionEvent event) {
+    }
+    
+}
